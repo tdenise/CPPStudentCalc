f0100fd7ce9a99a2ca92368fbb2142bc4899cd4c
Denise Tran <tdenise@users.noreply.github.com>
2018-01-19 09:36:55 -0800

    Add files via upload

---
+++ Student/src/Student.java
@@ -0,0 +1,47 @@
+
+public class Student {
+	private double expenses;
+	private String residency;
+	private double tuition;
+	private String housing;
+	private int mealPlan;
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	public void setHousing(String s) {
+		housing = s;
+	}
+	
+	public String getHousing() {
+		return housing;
+	}
+	
+	public void setMealPlan(int i) {
+		mealPlan = i;
+	}
+	
+	public int getMealPlan() {
+		return mealPlan;
+	}
+	
+	public void setTuition(String residency) {
+		residency = this.residency;
+	}
+	
+	public double getTuition() {
+		return tuition;
+	}
+	
+	public void displayingExpenses(double e) {
+		expenses = e;
+		System.out.println("Your total expenses are: " + expenses);
+	}
+	
+	public void calculateExpenses() {
+		
+	}	
+	
+	
+}
