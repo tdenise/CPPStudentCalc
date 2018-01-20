506f2703a46233f7749ee8c907b9b6252019a01a
joannacao <35121742+joannacao@users.noreply.github.com>
2018-01-19 17:49:49 -0800

    added displayStudents method
    created some students and called some methods. Can be update later when the Student class is finished

--- Student/src/Driver.java
+++ Student/src/Driver.java
@@ -2,6 +2,19 @@
 public class Driver {
   public static void main(String[] args){
     
+    Student hannah = new Student(14);
+    Student alex = new Student("In state"); 
+    
+    hannah.calculateExpenses(); 
+    alex.calculateExpenses();
+    hannah.displayExpenses(); 
+    
+    displayStudents(); 
+    
+  }
+  
+  public static void displayStudents(){
+    
   }
 
 }
