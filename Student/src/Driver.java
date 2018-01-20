034c0b019f0674d718aa9e7e3d7f6c56aa3c5874
tdenise <t.denise44@yahoo.com>
2018-01-19 20:24:17 -0800

    Deleted some methods

--- Student/src/Driver.java
+++ Student/src/Driver.java
@@ -2,17 +2,17 @@
 public class Driver {
 	public static void main(String[] args) {
 		Student s1 = new Student();
-		s1.setHousing("None");
+		s1.setHousing("Off Campus");
 		Student s2 = new Student();
 		s2.setHousing("Suites");
 		Student s3 = new Student();
-		s3.setHousing("None");
+		s3.setHousing("Dorms");
 		Student s4 = new Student();
 		s4.setHousing("None");		
 		Student s5 = new Student();
 		s5.setHousing("Village");
 		Student s6 = new Student();
-		s6.setHousing("None");
+		s6.setHousing("Off Campus");
 							
 	}
 	
--- Student/src/Student.java
+++ Student/src/Student.java
@@ -6,17 +6,6 @@ public class Student {
 	private String residency;
 	private String housing;
 	
-	public static void main(String[] args) {
-		
-	}
-
-	public Student(String house, int meal, String res, double tuit, double exp){
-		housing = house;
-		mealPlan = meal; 
-		residency = res; 
-		tuition = tuit;
-		expenses = exp;
-	}
 	public void setHousing(String s){
 		housing = s; 
 	}
@@ -31,19 +20,18 @@ public class Student {
 	}
 	public void setTuition(String residence){
 		
-		//nested if statements
-		//based on value of residence change value of tuition
+		
 	}
 	public double getTuition(){
 		return tuition; 
-	}
-	public void displayExpenses(){
-		System.out.println("Total Expenses: " + expenses);
 	}
 	public void calculateExpenses(){
 		//nested if statements for housing and increment to expenses
 		//nested if statements or switch case for meal plan and increment to expenses
 		expenses += tuition; 
+	}
+	public void displayExpenses(){
+		System.out.println("Total Expenses: " + expenses);
 	}
 	
 	
