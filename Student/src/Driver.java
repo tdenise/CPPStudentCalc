b046bb2e3d5275c9399f3163a3f040da492a9d94
tdenise <t.denise44@yahoo.com>
2018-01-19 19:34:44 -0800

    Set Housing

--- Student/src/Driver.java
+++ Student/src/Driver.java
@@ -1,4 +1,22 @@
 
 public class Driver {
-
+	public static void main(String[] args) {
+		Student s1 = new Student();
+		s1.setHousing("None");
+		Student s2 = new Student();
+		s2.setHousing("Suites");
+		Student s3 = new Student();
+		s3.setHousing("None");
+		Student s4 = new Student();
+		s4.setHousing("None");		
+		Student s5 = new Student();
+		s5.setHousing("Village");
+		Student s6 = new Student();
+		s6.setHousing("None");
+							
+	}
+	
+	public static void displayStudents() {
+		
+	}
 }
--- Student/src/Student.java
+++ Student/src/Student.java
@@ -1,47 +1,50 @@
 
 public class Student {
 	private double expenses;
-	private String residency;
 	private double tuition;
-	private String housing;
 	private int mealPlan;
+	private String residency;
+	private String housing;
 	
 	public static void main(String[] args) {
 		
 	}
-	
-	public void setHousing(String s) {
-		housing = s;
+
+	public Student(String house, int meal, String res, double tuit, double exp){
+		housing = house;
+		mealPlan = meal; 
+		residency = res; 
+		tuition = tuit;
+		expenses = exp;
 	}
-	
-	public String getHousing() {
-		return housing;
+	public void setHousing(String s){
+		housing = s; 
 	}
-	
-	public void setMealPlan(int i) {
-		mealPlan = i;
+	public String getHousing(){
+		return housing; 
 	}
-	
-	public int getMealPlan() {
-		return mealPlan;
+	public void setMealPlan(int i){
+		mealPlan = i; 
 	}
-	
-	public void setTuition(String residency) {
-		residency = this.residency;
+	public int getMealPlan(){
+		return mealPlan; 
 	}
-	
-	public double getTuition() {
-		return tuition;
-	}
-	
-	public void displayingExpenses(double e) {
-		expenses = e;
-		System.out.println("Your total expenses are: " + expenses);
-	}
-	
-	public void calculateExpenses() {
+	public void setTuition(String residence){
 		
-	}	
+		//nested if statements
+		//based on value of residence change value of tuition
+	}
+	public double getTuition(){
+		return tuition; 
+	}
+	public void displayExpenses(){
+		System.out.println("Total Expenses: " + expenses);
+	}
+	public void calculateExpenses(){
+		//nested if statements for housing and increment to expenses
+		//nested if statements or switch case for meal plan and increment to expenses
+		expenses += tuition; 
+	}
 	
 	
 }
