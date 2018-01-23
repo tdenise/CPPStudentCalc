10e63f25c835dfa2f1740a6d720dd74ded51e0d8
tdenise <t.denise44@yahoo.com>
2018-01-22 21:24:10 -0800

    Created new method displayStudents
    Displays total expenses, tuition, housing, and meal plan.

--- Student/src/Driver.java
+++ Student/src/Driver.java
@@ -1,18 +1,19 @@
 
 public class Driver {
-  public static void main(String[] args){
-    
-	Student hannah = new Student(14);
-	Student alex = new Student("In state");
+	public static void main(String[] args){
+		Student hannah = new Student(14);
+		Student alex = new Student("In state");
 	
-	hannah.displayExpenses(); 
-	alex.displayExpenses(); 
-    
-    	System.out.println("The following students are displayed in the following:");
-    	System.out.println("Hannah's Expense: " + hannah.getExpense() + "\tTuitiion: " + hannah.getTuition() + "\tHousing: " + hannah.getHousing() + "\tMeal Plan: " + hannah.getMealPlan());
-    	System.out.println("Alex's Expense: " + alex.getExpense() + "\tTuitiion: " + alex.getTuition() + "\tHousing: " + alex.getHousing() + "\tMeal Plan: " + alex.getMealPlan()); 
+		displayStudents(hannah, alex);
+
     
   }
+  
+	public static void displayStudents(Student hannah, Student alex){   
+		System.out.println("The following students are displayed in the following:");
+    	System.out.println("Hannah's Total Expenses: " + hannah.getExpense() + "\tTuitiion: " + hannah.getTuition() + "\tHousing: " + hannah.getHousing() + "\tMeal Plan: " + hannah.getMealPlan());
+    	System.out.println("Alex's Total Expenses: " + alex.getExpense() + "\tTuitiion: " + alex.getTuition() + "\tHousing: " + alex.getHousing() + "\tMeal Plan: " + alex.getMealPlan());
+    	}  
   
 
 }
