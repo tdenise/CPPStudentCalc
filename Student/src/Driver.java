ae89355fe9e1d8f32fe05fefc1a60c29bc3db5dd
tdenise <t.denise44@yahoo.com>
2018-01-23 00:00:49 -0800

    Typo in Driver
    tuition mispelled

--- Student/src/Driver.java
+++ Student/src/Driver.java
@@ -3,7 +3,8 @@ public class Driver {
 	public static void main(String[] args){
 		Student hannah = new Student(14);
 		Student alex = new Student("In state");
-	
+		
+		displayExpenses();
 		displayStudents(hannah, alex);
 
     
@@ -11,8 +12,8 @@ public class Driver {
   
 	public static void displayStudents(Student hannah, Student alex){   
 		System.out.println("The following students are displayed in the following:");
-    	System.out.println("Hannah's Total Expenses: " + hannah.getExpense() + "\tTuitiion: " + hannah.getTuition() + "\tHousing: " + hannah.getHousing() + "\tMeal Plan: " + hannah.getMealPlan());
-    	System.out.println("Alex's Total Expenses: " + alex.getExpense() + "\tTuitiion: " + alex.getTuition() + "\tHousing: " + alex.getHousing() + "\tMeal Plan: " + alex.getMealPlan());
+    	System.out.println("Hannah's Total Expenses: " + hannah.getExpense() + "\tTuition: " + hannah.getTuition() + "\tHousing: " + hannah.getHousing() + "\tMeal Plan: " + hannah.getMealPlan());
+    	System.out.println("Alex's Total Expenses: " + alex.getExpense() + "\tTuition: " + alex.getTuition() + "\tHousing: " + alex.getHousing() + "\tMeal Plan: " + alex.getMealPlan());
     	}  
   
 
